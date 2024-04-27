e3f157e4a1c87521c38b7c28b08c7ac1d7d9f9a7
Mario Cardia <mcardia@atlassian.com>
2024-04-27 09:45:47 -0300

    waitFor

---
+++ waitfor.js
@@ -0,0 +1,18 @@
+export function waitFor(selector) {
+  return new Promise((resolve, reject) => {
+    const e = document.querySelector(selector);
+    if (e) {
+      resolve(e);
+      return;
+    }
+    new MutationObserver((m, o) => {
+      Array.from(document.querySelectorAll(selector)).forEach((element) => {
+        resolve(element);
+        o.disconnect();
+      });
+    }).observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
